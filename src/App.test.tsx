65240055e139d08a6894f6e88dad9aa7db08cb45
bruce007lee <lizhen8_8@hotmail.com>
2022-05-24 20:15:51 +0800

    feat: add test case

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,26 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+const sleep = (time = 2000) =>
+  new Promise<void>((resovle) => {
+    setTimeout(() => {
+      resovle();
+    }, time);
+  });
+
+test('testSearchQQ', async () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const input = screen.getByPlaceholderText(/请输入qq号查询/i);
+
+  fireEvent.change(input, { target: { value: '156908838' } });
+  fireEvent.keyDown(input, { key: 'Enter' });
+
+  await waitFor(
+    () => {
+      const result = screen.getByText(/^kira∑$/);
+      expect(result).toBeInTheDocument();
+    },
+    { timeout: 3000 }
+  );
 });
